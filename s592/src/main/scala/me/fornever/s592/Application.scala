e483f4b281b6b7b7c8a852e62ce26ecae2c775cb
Friedrich von Never <friedrich@fornever.me>
2017-03-14 21:38:01 +0700

    s592: extract to external repository

--- s592/src/main/scala/me/fornever/s592/Application.scala
+++
@@ -1,9 +0,0 @@
-package me.fornever.s592
-
-object Application extends App {
-
-    args match {
-        case Array(filePath) => Processor.process(filePath)
-        case _ => println("Usage: <file> to process the file")
-    }
-}
